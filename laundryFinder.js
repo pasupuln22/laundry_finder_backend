ea68223a937d76b88d48c71321a3e351c9f5b949
NagarjunaP <pasupuln@roehampton.ac.uk>
2024-05-13 02:34:43 +0100

    contact api

--- laundryFinder.js
+++ laundryFinder.js
@@ -29,13 +29,25 @@ app.use(function (req, res, next) {
     next();
 });
 
-
+app.post('/contact', (req, res) => {
+    const { name, email, message } = req.body;
+    const sql = 'INSERT INTO ContactUs (name, email_id, message) VALUES (?, ?, ?)';
+    db.query(sql, [name, email, message], (err, result) => {
+        if (err) {
+            console.error('Error inserting into ContactUs table:', err);
+            res.status(500).json({ status: 500, message: 'Failed to insert into ContactUs table' });
+            return;
+        }
+        console.log('Inserted into ContactUs table:', result);
+        res.status(200).json({ status: 200, message: 'Contact details inserted successfully' });
+    });
+});
 
 app.use('/laundry_finder/autenticate', autenticationRoute);
 app.use('/laundry_finder/bookings', bookingsRoute);
 app.use('/laundry_finder/laundry', laundryRoute);
 app.use('/laundry_finder/tips', tipsRoute);
-const PORT = process.env.PORT || 3001; // Assuming default port is 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server connected to port ${PORT}`);
 });
